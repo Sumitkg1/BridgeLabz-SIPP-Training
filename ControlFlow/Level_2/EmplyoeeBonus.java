426e4e967ebce12fd1f4e6425a71f0d5557ffc8c
Sumit Kumar <sumitku2240@gmail.com>
2025-06-18 11:42:06 +0530

    [Sumit]  Refactor . Renaming Level_2 Completed

--- ControlFlow/Level_2/EmplyoeeBonus.java
+++ ControlFlow/Level_2/EmplyoeeBonus.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class EmplyoeeBonus {
 
-    
-     public static void main(String[] args) {
+
+     public static void main(String[] args) {  
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter your salary: ");
