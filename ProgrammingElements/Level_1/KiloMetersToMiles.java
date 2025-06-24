dea3dbf0d013f1c58aa935bf7bd7446f7a0e18b0
Sumit Kumar <sumitku2240@gmail.com>
2025-06-24 08:17:19 +0530

    [Sumit] Refactor . Delete Unwanted files

--- ProgrammingElements/Level_1/KiloMetersToMiles.java
+++
@@ -1,9 +0,0 @@
-package ProgrammingElements.Level_1;
-public class KiloMetersToMiles{
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers * 1.6;
-
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
-    }
-}
