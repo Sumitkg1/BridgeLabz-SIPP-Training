f9acc98e78e8b1b03bf64dac077eb03f0e9d34fc
Sumit Kumar <sumitku2240@gmail.com>
2025-07-03 23:15:23 +0530

    [Sumit] Add . EncapsulationPolymorphismInterfaceAbstract Sheet Done]

---
+++ EncapsulationPolymorphismInterfaceandAbstract/BankingSystem.java
@@ -0,0 +1,43 @@
+abstract class BankAccount {
+    private String accountNumber, holderName;
+    private double balance;
+
+    public BankAccount(String accountno, String name, double balance) {
+        this.accountNumber = accountno;
+        this.holderName = name;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) balance -= amount;
+    }
+
+    public abstract double calculateInterest();
+}
+
+interface Loanable {
+    void applyForLoan(double amount);
+    boolean calculateLoanEligibility();
+}
+
+class SavingsAccount extends BankAccount implements Loanable {
+    public SavingsAccount(String accountno, String name, double balance) {
+        super(accountno, name, balance);
+    }
+
+    public double calculateInterest() {
+        return 0.04 * balance;
+    }
+
+    public void applyForLoan(double amount) {
+        System.out.println("Applied for loan: " + amount);
+    }
+
+    public boolean calculateLoanEligibility() {
+        return true;
+    }
+}
---
+++ EncapsulationPolymorphismInterfaceandAbstract/Ecommerce.java
@@ -0,0 +1,41 @@
+abstract class Product {
+    private String productId, productname;
+    private double price;
+
+    public Product(String id, String productname, double price) {
+        this.productId = id;
+        this.productname = productname;
+        this.price = price;
+    }
+
+    public abstract double calculateDiscount();
+    public double getPrice() {
+        return price;
+    }
+    public String getproductname() {
+        return productname;
+    }
+}
+
+interface Taxable {
+    double calculateTax();
+    String getTaxDetails();
+}
+
+class Electronics extends Product implements Taxable {
+    public Electronics(String id, String productname, double price) {
+        super(id, productname, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.10;
+    }
+
+    public double calculateTax() {
+        return getPrice() * 0.18;
+    }
+
+    public String getTaxDetails() {
+        return "GST 18%";
+    }
+}
